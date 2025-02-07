48a66e0e7095c3f494f6026b9f116515e74f57bb
subhan-tahir <tsubhan822@gmail.com>
2025-02-07 12:44:01 +0500

    add log to check api key.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-
+console.log('api key',process.env.GOOGLE_MAP_API)
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
